b404048769b30ca0c818b1754b769b1fc855eb32
Atun <mali1996ua@gmail.com>
2022-10-11 22:27:32 +0300

    mixins added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello</h1>
       <Navbar />
       <Header />
+      <Main />
     </div>
   );
 }
